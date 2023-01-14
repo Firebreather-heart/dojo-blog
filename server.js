045e5debfd3598c983b517f1007936093b50a2b3
firebreather <dtenny95@gmail.com>
2023-01-14 15:00:51 +0100

    deploy to vercel

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const jsonServer = require('json-server')
+const clone = require('clone')
+const data = require('./db.json')
+
+const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
-const router = jsonServer.router('./data/db.json')
+
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+    _isFake: isProductionEnv
+})
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+
+server.use((req, res, next) => {
+    if (req.path !== '/')
+        router.db.setState(clone(data))
+    next()
+})
+
 server.use(router)
-server.listen(3000, () => {
+server.listen(process.env.PORT || 8000, () => {
     console.log('JSON Server is running')
 })
 
